bfbeb657077f2461a94c42df7003084344a4ae1b
julianC <mantillajulian234@gmail.com>
2023-04-12 15:54:04 -0500

    termino docker compose como herramienta de desarrollo

--- docker/index.js
+++ docker/index.js
@@ -10,9 +10,9 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 app.get('/', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('💥 HOLA MAFEww 💥: ' + err);
+      res.status(500).send('💥 HOLA MAFE ALVARADO 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('!Me conecté a la DB de Platzi! 😎');
       db.close();
     }
   });
